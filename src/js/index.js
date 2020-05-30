9b287f8633a26d99c66e095894d2d1a4a0e37a51
eduardo reta rj <https://github.com/Eddie-Reta>
2020-05-30 00:37:27 -0400

    list of items populate page

--- src/js/index.js
+++ src/js/index.js
@@ -2,8 +2,54 @@
 
 import Search from "./models/Search";
 
-const search = new Search("pizza");
+import * as searchView from "./views/searchView";
 
-console.log(search)
+import { elements } from "./views/base";
 
-search.getResults();
+//global state of the app
+/*
+    search object 
+    current recipe object
+    shopping list object
+    liked recipes
+
+*/
+
+// event listeners going in the controller
+
+const state = {};
+
+const controlSearch = async () => {
+    //1. get the query from the view
+    
+    const query = searchView.getInput();
+    console.log(query)
+
+    if (query) {
+        //2) New search object and add to state
+        state.search = new Search(query);
+
+    };
+
+    //3) prepare Ui for results
+    searchView.clearInput();
+    searchView.clearResults();
+    //4 search for recipes
+
+    await state.search.getResults();
+
+    // render results on UI
+    searchView.renderResults(state.search.result)
+};
+
+document.querySelector(".search").addEventListener("submit", e => {
+    e.preventDefault();
+    controlSearch();
+});
+
+// const search = new Search("pizza");
+
+// console.log(search)
+
+// search.getResults();
+
